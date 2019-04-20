4319ec6075bffd815cd2ec210b3992a2e42ea048
obentham <oliverbentham@gmail.com>
2019-04-20 19:41:20 -0400

    single view app

---
+++ semesterProjectGroup23/semesterProjectGroup23/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  semesterProjectGroup23
+//
+//  Created by Oliver Bentham on 4/20/19.
+//  Copyright © 2019 Oliver Bentham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
